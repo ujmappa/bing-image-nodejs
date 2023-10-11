53cc9c185022dd7b2370c110a1df267a5b9e7a30
ujmappa <79494755+ujmappa@users.noreply.github.com>
2023-10-11 14:23:59 +0200

    Minor readability changes

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import {generateImagesLinks} from 'bimg';
 import {downloadFile} from './download.js';
 
 import fs from 'node:fs';
-import path from 'path';
+import path from 'node:path';
+
 import * as uuid from 'uuid';
+
 
 (async () => {
 	const content = fs.readFileSync('./prompts.txt', {encoding: 'utf-8'});
@@ -15,12 +17,12 @@ import * as uuid from 'uuid';
 		try {
 			const links = await generateImagesLinks(prompt.trim());
 			for (const link of links) {
-				const uuidv4 = uuid.v4(); // crypto.randomUUID();
-				const filename = prompt.replace(/[^a-z0-9]/gi, '_').substring(0, 192-5-uuidv4.length) + '_' + uuidv4 + '.jpg';
+				const uuidv4 = uuid.v4(); // Replaced crypto.randomUUID() to work with older versions of NodeJS as well
+				const filename = `${prompt.replace(/[^a-z0-9]/gi, '_').substring(0, 192-5-uuidv4.length)}_${uuidv4}.jpg`;
 				await downloadFile(link, path.join(path.normalize(process.env.DESTINATION_DIR || './images'), filename));
 			}
 		} catch(error) {
-			console.error(error);
+			console.error(error.message);
 		}
 	}
 })();
